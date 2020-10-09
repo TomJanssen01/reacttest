94604b9356c5207dd556881ab31aeb529acc31d9
Tom Janssen <tom.janssen@student.thomasmore.be>
2020-10-09 11:13:50 +0200

    Updates

---
+++ precache-manifest.bc1bb6e2147b9d71da8ffffb931e7fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3a67e020899ecc4c58075509c529ca",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "1182899a1b0252bb64b9",
+    "url": "/reacttest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1307bc5a5bb1fb114252",
+    "url": "/reacttest/static/js/2.c9b88508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttest/static/js/2.c9b88508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1182899a1b0252bb64b9",
+    "url": "/reacttest/static/js/main.1b528ae4.chunk.js"
+  },
+  {
+    "revision": "7b65aa31a6ca0391dad8",
+    "url": "/reacttest/static/js/runtime-main.82a5519d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
